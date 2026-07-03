5c210f0063f5115e3d120ce7af82004a91ea623e
unknown <explorerkd@gmail.com>
2026-07-03 17:12:45 +0530

    All Airports Updated_2

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../src/images/logo.png";
+import logo from "../src/images/logo.webp";
 
 const styles = {
   nav: {
